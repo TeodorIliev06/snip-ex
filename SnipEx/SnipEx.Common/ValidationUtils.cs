fcfc09a661621977bfb8406227c59f63dcfc2002
TeodorIliev06 <tedo2505@gmail.com>
2025-03-05 18:00:39 +0200

    Add guid parse util

---
+++ SnipEx/SnipEx.Common/ValidationUtils.cs
@@ -0,0 +1,28 @@
+﻿namespace SnipEx.Common
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public static class ValidationUtils
+    {
+        public static bool TryGetGuid(string? id, out Guid guid)
+        {
+            if (String.IsNullOrWhiteSpace(id))
+            {
+                guid = Guid.Empty;
+                return false;
+            }
+
+            return Guid.TryParse(id, out guid);
+        }
+
+        public static bool IsValid(object obj)
+        {
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            var context = new ValidationContext(obj);
+            var isValid = Validator.TryValidateObject(obj, context, validationResults);
+
+            return isValid;
+        }
+    }
+}
