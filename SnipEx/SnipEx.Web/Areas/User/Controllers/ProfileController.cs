da5809c3f56f0fc1269d842ac31e978162dd219b
TeodorIliev06 <tedo2505@gmail.com>
2025-03-26 17:14:11 +0200

    Introduce an user area

---
+++ SnipEx/SnipEx.Web/Areas/User/Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SnipEx.Web.Areas.User.Controllers
+{
+    [Area("User")]
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
