a107fc9bb2b59086694ef8c78271576c8ec0da1b
Teodor <tedo2505@gmail.com>
2025-08-18 12:32:22 +0300

    feat(ui): restore external login options

--- SnipEx/SnipEx.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SnipEx/SnipEx.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -32,6 +32,12 @@ namespace SnipEx.Web.Areas.Identity.Pages.Account
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
         public string ReturnUrl { get; set; }
 
         /// <summary>
@@ -83,12 +89,16 @@ namespace SnipEx.Web.Areas.Identity.Pages.Account
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
+            ExternalLogins = (await signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
             ReturnUrl = returnUrl;
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
+
+            ExternalLogins = (await signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
             if (ModelState.IsValid)
             {
